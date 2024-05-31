889caae2178e3523563c98eb87d9afe4b3bf4693
JhonaLude <jhonathan.ludena@unl.edu.ec>
2024-05-30 20:34:40 -0500

    claseSuma
    30/05/2024

---
+++ Clases/src/clases/SumaDosNumeros.java
@@ -0,0 +1,10 @@
+public class SumaDosNumeros {
+    public static void main(String[] args) {
+        int numero1 = 5;
+        int numero2 = 7;
+        
+        int suma = numero1 + numero2;
+        
+        System.out.println("La suma de " + numero1 + " y " + numero2 + " es: " + suma);
+    }
+}
